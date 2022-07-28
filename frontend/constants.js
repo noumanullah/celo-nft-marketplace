5856f484c54691839f747a20f607799d98b51bc1
Nouman Ullah Siddiqui <nullah@macrosoftinc.com>
2022-07-28 05:50:38 +0500

    NextJS Website completed

---
+++ frontend/constants.js
@@ -0,0 +1,4 @@
+export const SUBGRAPH_URL =
+    "https://api.thegraph.com/subgraphs/name/noumanullah/celo-nft-marketplace";
+
+export const MARKETPLACE_ADDRESS = "0xD6C28e3c144374B77e0387A68D0AD2d6073Cf814";
